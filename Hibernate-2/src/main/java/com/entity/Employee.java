1bd97a9cc6bca86e14a9dab320484b58d4e0b898
ShubhamKRay <shubhairmen@gmail.com>
2026-03-08 00:45:18 +0530

    create table using xml file in which Entity class

---
+++ Hibernate-2/src/main/java/com/entity/Employee.java
@@ -0,0 +1,73 @@
+package com.entity;
+//ENTITY
+public class Employee {
+
+	private int eid;
+	private String ename;
+	private String eaddress;
+	private int esalary;
+	
+	
+	public Employee() {
+		// Hibernate will use this Constructor
+	}
+
+
+	public Employee(int eid, String ename, String eaddress, int esalary) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.eaddress = eaddress;
+		this.esalary = esalary;
+	}
+
+
+	public int getEid() {
+		return eid;
+	}
+
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+
+	public String getEname() {
+		return ename;
+	}
+
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+
+	public String getEaddress() {
+		return eaddress;
+	}
+
+
+	public void setEaddress(String eaddress) {
+		this.eaddress = eaddress;
+	}
+
+
+	public int getEsalary() {
+		return esalary;
+	}
+
+
+	public void setEsalary(int esalary) {
+		this.esalary = esalary;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", eaddress=" + eaddress + ", esalary=" + esalary + "]";
+	}
+	
+	
+	
+	
+}
